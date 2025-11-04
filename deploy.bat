dedafffce211489f970f2afd86ce99b0950c9492
Jonas van Leeuwen <jonas@yixboost.dev>
2025-11-04 13:31:59 +0100

    Add deploy automation files

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+@echo off
+set DEPLOYMENT_BRANCH=gh-pages
+set GIT_USER=jonasvanleeuwen19
+set USE_SSH=false
+
+npm run deploy
+pause
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DEPLOYMENT_BRANCH=gh-pages
+export GIT_USER=jonasvanleeuwen19
+export USE_SSH=false
+
+npm run deploy
